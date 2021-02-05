337ab57c20cbc33073fd1d77f55d39ac6bd5583b
Masora <masora@menhera.org>
2021-02-05 22:14:20 +0900

    non-tested updates

--- background.mjs
+++ background.mjs
@@ -1,22 +1,11 @@
 // vim: ts=4 noet ai
 
-import {SetMap} from '/lib/SetMap.mjs';
-
-const tabsByWindow = new SetMap;
-const windowByTab = new Map;
-
-const activeTabByWindow = new Map;
 const activeDomainByWindow = new Map;
-const tabInfo = new Map;
 
 const registrableDomains = new Set;
 
-browser.runtime.onMessage.addListener((message) => {
-	if (message.command == 'registrable_domain') {
-		console.log('registrable domain:', message.domain);
-		registrableDomains.add(message.domain);
-	}
-});
+const disabledWindows = new Set;
+
 
 globalThis.getRegistrableDomain = (aDomain) =>
 {
@@ -31,50 +20,11 @@ globalThis.getRegistrableDomain = (aDomain) =>
 	return domain;
 };
 
-const getActiveUrl = (windowId) => {
-	if (!activeTabByWindow.has(windowId)) {
-		return 'about:blank';
-	}
-	const activeTabId = activeTabByWindow.get(windowId);
-	if (!tabInfo.has(activeTabId)) {
-		return 'about:blank';
-	}
-	return tabInfo.get(activeTabId).url;
-};
-
-const addTab = (tab) =>
-{
-	const url = tab.url || 'about:blank';
-	const origin = new URL(url).origin;
-	const domain = origin == 'null' ? null : new URL(origin).hostname;
-	tabInfo.set(tab.id, {
-		id: tab.id,
-		origin,
-		domain,
-		url,
-	});
-	return tabInfo.get(tab.id);
-};
-
 globalThis.getDomain = (aUrl) =>
 {
 	const url = aUrl || 'about:blank';
 	const origin = new URL(url).origin;
 	return origin == 'null' ? null : new URL(origin).hostname;
-};
-
-globalThis.updateTabData = async () =>
-{
-	//
-	const tabs = await browser.tabs.query({});
-	for (const tab of tabs) {
-		const domain = getDomain(tab.url);
-		if (tab.active) {
-			if (domain) {
-				activeDomainByWindow.set(tab.windowId, domain);
-			}
-		}
-	}
 };
 
 globalThis.getActiveDomainByWindow = (windowId) =>
@@ -84,99 +34,99 @@ globalThis.getActiveDomainByWindow = (windowId) =>
 		: getRegistrableDomain(activeDomainByWindow.get(windowId));
 };
 
+globalThis.getNoHideModeByWindow = (windowId) =>
+	disabledWindows.has(windowId);
 
-window.tabController = {
-	//
-	async updateTabData()
-	{
-		//
-		const tabs = await browser.tabs.query({});
-		for (const tab of tabs) {
-			const {domain} = addTab(tab);
-			if (tab.active) {
-				if (domain) {
-					activeDomainByWindow.set(tab.windowId, domain);
-				}
-				tabInfoactiveTabByWindow.set(tab.windowId, tab.id);
-			}
-			tabsByWindow.set(tab.windowId, tab.id);
-			windowByTab.set(tab.id, tab.windowId);
-		}
-	},
-	getActiveDomainByWindow(windowId)
-	{
-		return !activeDomainByWindow.has(windowId)
-			? null
-			: getRegistrableDomain(activeDomainByWindow.get(windowId));
-	},
-	setActiveDomainByWindow(windowId, origin)
-	{
-		//
-	},
-	async getDomainsByWindow(windowId)
-	{
-		const tabs = await browser.tabs.query({windowId});
-		for (const tab of tabs) {
-
-		}
-	},
-	async updateShownTabOnWindow(windowId)
-	{
-		//
-		const activeUrl = getActiveUrl(windowId);
-		const activeOrigin = new URL(activeUrl).origin;
-		const activeDomain = tabController.getActiveDomainByWindow(windowId);
-		const tabs = await browser.tabs.query({windowId});
-		const promises = [];
-		for (const tab of tabs) {
-			if (tab.pinned) continue;
-			const url = new URL(tab.url);
-			const origin = url.origin;
-			if ('null' === origin) {
-				promises.push(browser.tabs.show(tab.id));
-				continue;
-			}
-			const domain = new URL(origin).hostname;
-			const registrableDomain = getRegistrableDomain(domain);
-			if (origin !== activeOrigin) {
-				promises.push(browser.tabs.hide(tab.id));
-			} else {
-				promises.push(browser.tabs.show(tab.id));
-			}
-		}
-		await Promise.all(promises);
-	},
+globalThis.setNoHideModeByWindow = async (windowId, noHide) =>
+{
+	if (noHide) {
+		disabledWindows.add(windowId);
+	} else {
+		disabledWindows.delete(windowId);
+	}
+	await updateShownTabByWindow(windowId);
 };
 
-browser.tabs.onCreated.addListener((tab) => {
-	tabsByWindow.set(tab.windowId, tab.id);
-	windowByTab.set(tab.id, tab.windowId);
-	if (tab.active) {
-		activeTabByWindow.set(tab.windowId, tab.id);
+globalThis.setActiveDomainByWindow = async (windowId, domain) =>
+{
+	activeDomainByWindow.set(windowId, domain);
+	await updateShownTabByWindow(windowId);
+};
+
+globalThis.getDomainsByWindow = async (windowId) =>
+{
+	const tabs = await browser.tabs.query({windowId});
+	const domains = new Set;
+	for (const tab of tabs) {
+		const domain = getRegistrableDomain(getDomain(tab.url));
+		if (domain) {
+			domains.add(domain);
+		}
 	}
-});
+	return [... domains].sort();
+};
 
-browser.tabs.onRemoved.addListener((tabId, removeInfo) => {
-	const {isWindowClosing, windowId} = removeInfo;
-	tabInfo.delete(tabId);
-	tabsByWindow.deleteValue(windowId, tabId);
-	windowByTab.delete(tabId);
-	if (isWindowClosing) {
-		tabsByWindow.delete(windowId);
+globalThis.updateShownTabByWindow = async (windowId) =>
+{
+	const noHide = getNoHideModeByWindow(windowId);
+	const activeDomain = getActiveDomainByWindow(windowId);
+	const tabs = await browser.tabs.query({windowId});
+	const promises = [];
+	const activeTabs = [];
+	for (const tab of tabs) {
+		if (tab.pinned) continue;
+		if (!activeDomain) {
+			promises.push(browser.tabs.show(tab.id));
+			continue;
+		}
+		const domain = getDomain(tab.url);
+		if (!domain) {
+			promises.push(browser.tabs.show(tab.id));
+			continue;
+		}
+		const registrableDomain = getRegistrableDomain(domain);
+		if (activeDomain !== registrableDomain) {
+			if (noHide) {
+				promises.push(browser.tabs.show(tab.id));
+			} else {
+				promises.push(browser.tabs.hide(tab.id));
+			}
+		} else {
+			promises.push(browser.tabs.show(tab.id));
+			if (noHide) {
+				activeTabs.push(tab.id);
+			}
+		}
 	}
-})
 
-browser.tabs.onDetached.addListener((tabId, detachInfo) => {
-	tabsByWindow.deleteValue(detachInfo.oldWindowId, tabId);
-});
+	if (noHide && activeTabs.length) {
+		promises.push(browser.tabs.highlight(activeTabs[activeTabs.length - 1]));
+	}
+	await Promise.all(promises);
+};
 
-browser.tabs.onAttached.addListener(async (tabId, {newWindowId}) => {
-	try {
-		//
-		tabsByWindow.set(newWindowId, tabId);
-		windowByTab.set(tabId, newWindowId);
-	} catch (e) {
-		console.error(e);
+globalThis.updateTabData = async () =>
+{
+	const tabs = await browser.tabs.query({});
+	for (const tab of tabs) {
+		const domain = getDomain(tab.url);
+		if (tab.active) {
+			if (domain) {
+				activeDomainByWindow.set(tab.windowId, domain);
+				console.log('Active domain on window:', tab.windowId, domain);
+			}
+		}
+	}
+};
+
+
+browser.runtime.onMessage.addListener((message) => {
+	if (message.command == 'registrable_domain') {
+		const {domain} = message;
+		if (!registrableDomains.has(domain)) {
+			console.log('registrable domain:', domain);
+		}
+		registrableDomains.add(domain);
 	}
 });
 
@@ -186,18 +136,13 @@ browser.tabs.onUpdated.addListener(async (tabId, changeInfo, updatedTab) => {
 			if (updatedTab.discarded || !updatedTab.url) {
 				// ignore
 			} else {
-				const url = updatedTab.url;
-				const origin = new URL(url).origin;
-				if ('null' === origin) {
-					console.log('null origin:', url);
-				}
-				//if (url !== 'about:blank')
-				const {domain} = addTab(updatedTab);
+				const domain = getDomain(updatedTab.url);
 				if (updatedTab.active) {
 					if (domain) {
 						activeDomainByWindow.set(updatedTab.windowId, domain);
+						console.log('Active tab updated on window:', updatedTab.windowId, domain);
 					}
-					await tabController.updateShownTabOnWindow(updatedTab.windowId);
+					await updateShownTabByWindow(updatedTab.windowId);
 				}
 			}
 			
@@ -207,15 +152,14 @@ browser.tabs.onUpdated.addListener(async (tabId, changeInfo, updatedTab) => {
 	}
 });
 
-tabController.updateTabData();
-
 browser.tabs.onActivated.addListener(async ({previousTabId, tabId, windowId}) => {
 	const activeTab = await browser.tabs.get(tabId);
-	activeTabByWindow.set(windowId, tabId);
 	if (activeTab.discarded || activeTab.url === 'about:blank') return;
-	const activeOrigin = new URL(activeTab.url).origin;
-	console.log('active origin:', activeOrigin, activeTab.url);
-	await tabController.updateShownTabOnWindow(windowId);
-	
+	const domain = getDomain(activeTab.url);
+	activeDomainByWindow.set(windowId, domain);
+	console.log('Tab activated on window:', windowId, domain);
+	await updateShownTabByWindow(windowId);
 });
+
+updateTabData();
 
--- popup/popup.mjs
+++ popup/popup.mjs
@@ -1,45 +1,59 @@
 // vim: ts=4 noet ai
 
-import {OriginStore} from '/lib/OriginStore.mjs';
-
 (async () => {
 	//
-	const tabManager = (await browser.runtime.getBackgroundPage()).tabManager;
-	const domainsElement = document.querySelector('#domains') || document.body;
+	const {
+		getActiveDomainByWindow,
+		getNoHideModeByWindow,
+		setNoHideModeByWindow,
+		setActiveDomainByWindow,
+		getDomainsByWindow,
+	} = (await browser.runtime.getBackgroundPage());
+
+	const menuListElement = document.querySelector('#menuList');
 	const currentWindow = await browser.window.getCurrent();
 	const windowId = currentWindow.id;
-	const tabs = await browser.tabs.query({windowId: browser.windows.WINDOW_ID_CURRENT});
-	const origins = new OriginStore;
-	for (const tab of tabs) {
-		origins.add(tab.url, tab);
+	
+	const toggleShowAllElement = document.createElement('li');
+	toggleShowAllElement.append('Show all tabs');
+	menuListElement.append(toggleShowAllElement);
+	if (getNoHideModeByWindow(windowId)) {
+		toggleShowAllElement.classList.add('checked');
 	}
-
-	{
-		const li = document.createElement('li');
-		li.append('Show all tabs');
-		domainsElement.append(li);
-		domainsElement.append(document.createElement('hr'));
-	}
-
-	for (const origin of origins) {
-		const li = document.createElement('li');
-		if ('null' === origin) {
-			li.append('Special pages');
+	toggleShowAllElement.addEventListener('click', ev => {
+		if (getNoHideModeByWindow(windowId)) {
+			toggleShowAllElement.classList.remove('checked');
+			setNoHideModeByWindow(windowId, false).catch(e => {
+				console.error(e);
+			});
 		} else {
-			const url = new URL(origin);
-			if ('https:' === url.protocol) {
-				li.classList.add('origin-secure');
-			}
-			li.append(url.host);
+			toggleShowAllElement.classList.add('checked');
+			setNoHideModeByWindow(windowId, true).catch(e => {
+				console.error(e);
+			});
 		}
-		domainsElement.append(li);
-		const key = origin;
+	});
+
+	const domains = await getDomainsByWindow(windowId);
+
+	if (domains.length) {
+		menuListElement.append(document.createElement('hr'));
+	}
+
+	const activeDomain = getActiveDomainByWindow(windowId);
+
+	for (const domain of domains) {
+		if (!domain) {
+			continue;
+		}
+		const li = document.createElement('li');
+		menuListElement.append(li);
+		li.append(domain);
+		if (activeDomain === domain) {
+			li.classList.add('checked');
+		}
 		li.addEventListener('click', ev => {
-			//
-			const matchedTabs = origins.get(key).map(tab => tab.index);
-			browser.tabs.highlight({tabs: matchedTabs, populate: false}).then(() => {
-				console.log('Origin shown:', key);
-			}).catch(e => {
+			setActiveDomainByWindow(windowId, domain).catch(e => {
 				console.error(e);
 			});
 		});
